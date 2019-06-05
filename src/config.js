936476acdc55183478f5667cb39b72147f137b22
Scott Williams <scottcolemanwilliams@gmail.com>
2019-06-05 13:18:30 -0500

    inital deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api",
-  TOKEN_KEY: "anyOtherToken" // process.env.REACT_APP_API_KEY
+  API_ENDPOINT: "https://salty-falls-98776.herokuapp.com/api", //"http://localhost:8000/api",
+  TOKEN_KEY: process.env.REACT_APP_API_KEY || "anyOtherToken" // process.env.REACT_APP_API_KEY
 };
