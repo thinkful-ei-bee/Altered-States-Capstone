02a37b96023a8800a4953c4c789f1e707293f0c8
Loxphordex <silasishallahan@gmail.com>
2019-06-06 08:26:26 -0700

    adding face chart

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,37 +1,13 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend,
-  PieChart, Pie, Sector, Cell } from 'recharts'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts'
 import "./DashboardRoute.css";
 
 
 class DashboardRoute extends Component {
 
   render() {
-    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-    const RADIAN = Math.PI / 180;
-
-
-    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-      const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-      const x  = cx + radius * Math.cos(-midAngle * RADIAN);
-      const y = cy  + radius * Math.sin(-midAngle * RADIAN);
     
-      return (
-        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">
-          {`${(percent * 100).toFixed(0)}%`}
-        </text>
-      );
-    };
-
-    const pieData = [
-      { name: 'Group A', value: 400 },
-      { name: 'Group B', value: 300 },
-      { name: 'Group C', value: 300 },
-      { name: 'Group D', value: 200 },
-    ];
-
-
     const data = [
       {name: '05/17', joy: 3.4, sadness: 4.4},
       {name: '05/18', joy: 1.4, sadness: 5.0},
@@ -53,28 +29,6 @@ class DashboardRoute extends Component {
             <YAxis domain={[1, 5]} />
           </LineChart>
         </div>
-
-    	<PieChart width={400} height={200} onMouseEnter={this.onPieEnter}>
-        <Pie
-          data={pieData} 
-          cx={300} 
-          cy={200} 
-          labelLine={false}
-          label={renderCustomizedLabel}
-          outerRadius={80} 
-          fill="#8884d8"
-          dataKey='value'
-        >
-        	{
-          	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
-          }
-        </Pie>
-      </PieChart>
-
-        <div className='mood-avg'>
-
-        </div>
-        
 
         <Link to="/new" className="new-entry-button">
             New Entry
