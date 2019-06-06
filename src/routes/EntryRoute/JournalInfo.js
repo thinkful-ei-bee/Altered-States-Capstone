b38ca6aa0ac7322113579949e1394f18e8346a24
Loxphordex <silasishallahan@gmail.com>
2019-06-06 11:35:23 -0700

    changed data

--- src/routes/EntryRoute/JournalInfo.js
+++ src/routes/EntryRoute/JournalInfo.js
@@ -23,10 +23,9 @@ export default class JournalInfo extends React.Component {
     };
 
     const data = [
-      {name: 'Group A', value: 400}, 
-      {name: 'Group B', value: 300},
-      {name: 'Group C', value: 300}, 
-      {name: 'Group D', value: 200}
+      {name: 'Joy', value: 400}, 
+      {name: 'Analytical', value: 300},
+      {name: 'Tentative', value: 300}
     ];
     return (
       <section className='journal-feedback'>
