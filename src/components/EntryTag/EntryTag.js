cffaf588ac5c08d6c5d5fa0226ac67c4f2b14fa1
Scott Williams <scottcolemanwilliams@gmail.com>
2019-06-12 09:51:57 -0500

    fixed dayOfWeek Entry Tag

--- src/components/EntryTag/EntryTag.js
+++ src/components/EntryTag/EntryTag.js
@@ -8,8 +8,12 @@ export default class EntryTag extends Component{
         const {date}=this.props
        
         const day = new Date(date).toLocaleString([], { hour12: true});
+
+        const dayTwo = new Date(date)
+
+        const dayOfWeek = dayTwo.toString().slice(0, 4)
         
-        const newDate = day.slice(0,-6) + day.slice(-3)     
+        const newDate = dayOfWeek + day.slice(0,-6) + day.slice(-3)     
         
         return <h3 className='entry-tag'>{newDate}</h3>
     }
