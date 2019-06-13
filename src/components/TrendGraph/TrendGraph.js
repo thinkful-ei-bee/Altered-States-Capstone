e752b7103d32f887ab97e21fc8099ac3e4e964e0
Enrique Montemayor <eemontemayor@live.com>
2019-06-13 16:05:51 -0500

    fixed trend graphs for mobile and desktop

--- src/components/TrendGraph/TrendGraph.js
+++ src/components/TrendGraph/TrendGraph.js
@@ -24,19 +24,22 @@ export default class TrendGraph extends Component{
     render(){
         const isMobile = window.innerWidth < 760;
         const width = isMobile ? 400 : 900;
-     
+        const height = isMobile ? 125 : 175;
 
         return(
-            <ResponsiveContainer  width='100%' height='100%'>
+            <div className='graph'>
+
+            <ResponsiveContainer  width='100%' height='100%' margin={{top:15}}>
             <div>{this.props.className} 
 
-            <LineChart width={width} height={175} data={this.props.data}>
+            <LineChart width={width} height={height} data={this.props.data} margin={{right:40, top:5}}>
             <Line type='monotone' dataKey={this.props.dataKey} stroke='#8884d8' strokeWidth={2} />
-            <YAxis ticks={this.props.className ==='happiness'?[0,1,2,3,4,5]:[0,1,2,3,4,5]} type='number' domain={[0, 5]} />
-            <XAxis dataKey='name'/>
+            <YAxis interval={0} ticks={this.props.className ==='happiness'?[0,1,2,3,4,5]:[0,1,2,3,4,5]} type='number' domain={[0, 5]} />
+            <XAxis  interval={0} dataKey='name'/>
           </LineChart>
             </div>
             </ResponsiveContainer>
+            </div>
         )
     }
 }
