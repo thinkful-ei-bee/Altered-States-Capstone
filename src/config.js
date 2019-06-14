2501169a995d022cae7dd6de3f472c90d34765b4
Scott Williams <scottcolemanwilliams@gmail.com>
2019-06-14 18:52:11 -0500

    endpoint fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
-  // ttps://alt-states-mimood.now.sh/login
-  API_ENDPOINT: "http://localhost:8000/api",
+  // https://alt-states-mimood.now.sh/login
+  // API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://salty-falls-98776.herokuapp.com/api",
   TONE_ANALYZER_ENDPOINT: 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21',
   TOKEN_KEY: "anyOtherToken", // process.env.REACT_APP_API_KEY
   TONE_ANALYZER_KEY: 'iuBn-mTorlcGgfLdUn4ct2Y91Rs-5ZFPqSmHzqadTaQ0',
