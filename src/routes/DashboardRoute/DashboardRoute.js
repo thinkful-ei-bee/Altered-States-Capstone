2b71ce7680185f07c459cc760e98782d68044c53
Scott Williams <scottcolemanwilliams@gmail.com>
2019-06-18 15:55:48 -0500

    entire entry card clickable, transition on hover and click

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -33,9 +33,15 @@ class DashboardRoute extends Component {
   }
 
 
+  // generateEntryLabel(entry) {
+  //   return <Link to={`/entry/${entry.id}`} ><EntryTag date={entry.date_created} /></Link>
+  // }
+
   generateEntryLabel(entry) {
-    return <Link to={`/entry/${entry.id}`} ><EntryTag date={entry.date_created} /></Link>
+    return <EntryTag date={entry.date_created} />
   }
+
+  
 
   renderEntryList() {
     const { entries } = this.state
@@ -56,9 +62,13 @@ class DashboardRoute extends Component {
   renderEntry(entry) {
     const selfie = entry.face_url ? true : false;
     return (
+    
       <li key={entry.id} className='entryList-entry'>
-        <EntryCharts entry={entry} label={this.generateEntryLabel(entry)} selfie={selfie}/>
+        <Link to={`/entry/${entry.id}`} >
+          <EntryCharts entry={entry} label={this.generateEntryLabel(entry)} selfie={selfie}/>
+        </Link>
       </li>
+      
     )
   }
 
